85f8d92a27b47c5221d030cfbda21db21649066e
codegirlly <coderly2017@163.com>
2019-01-28 21:30:30 +0800

    add zuul

---
+++ api-gateway/src/main/java/com/susoft/controller/GatewayController.java
@@ -0,0 +1,30 @@
+package com.susoft.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cloud.client.ServiceInstance;
+//import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * <p>  </p>
+ *
+ * @author ly
+ * @since 2019/1/16
+ */
+@RestController
+public class HouseController {
+
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
+
+    @RequestMapping(value="/house",method = RequestMethod.GET)
+    public String getHouse(){
+//        ServiceInstance instance = discoveryClient.getLocalServiceInstance();
+//        logger.info("/hello,host:"+instance.getHost()+",service_id:"+instance.getServiceId());
+        return "Hello World";
+    }
+
+}
